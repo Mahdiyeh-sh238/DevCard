a9dbdd7cefda9455492d827bec5c9c24148745a3
shefaat23879 <mahdiyehshefaat@gmail.com>
2021-01-25 07:56:11 -0800

    projects section from index page moved to partial view Projects

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+
+    }
+}
---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public String Client{ get; set; }
+
+    }
+}
